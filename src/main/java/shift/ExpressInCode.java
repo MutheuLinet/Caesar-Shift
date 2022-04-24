e1264a106b8eb4892c193dccba6252a0ed0eccbd
MutheuLinet <muneenilinet@gmail.com>
2022-04-24 15:43:09 +0300

    Add for loop to convert regular string text to char primitive type

--- src/main/java/shift/ExpressInCode.java
+++ src/main/java/shift/ExpressInCode.java
@@ -8,7 +8,23 @@ public class ExpressInCode {
 
         if (shiftKey < 1 || shiftKey > 25) throw new Exception("The shift key must be between 1 and 25");
 
+        for (int i=0; i<l; i++){
 
+            char letter = regularText.charAt(i);
+
+            if (Character.isLetter(letter)){
+                if (Character.isUpperCase(letter)) {
+                    char shiftedLetter = (char) (letter + shiftKey);
+                    if (shiftedLetter > 'Z') {
+                        expressedInCode += (char) (letter - (26 - shiftKey));
+                    } else {
+                        expressedInCode += shiftedLetter;
+                    }
+                }
+            }else {
+                expressedInCode += letter;
+            }
+        }
         return expressedInCode;
     }
 
