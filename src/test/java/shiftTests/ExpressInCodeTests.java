ee775f9e51a11dd86ef0e105b02d7368978beee6
MutheuLinet <muneenilinet@gmail.com>
2022-04-24 17:02:21 +0300

    Rename puttingToCode to putToCode

--- src/test/java/shiftTests/ExpressInCodeTests.java
+++ src/test/java/shiftTests/ExpressInCodeTests.java
@@ -10,7 +10,7 @@ public class ExpressInCodeTests {
     @Test
     public void ExpressInCode_onlyActualStrings_Exception() throws Exception {
         ExpressInCode expressInCode = new ExpressInCode();
-        String inputted = expressInCode.puttingIntoCode("123", 1);
+        String inputted = expressInCode.putIntoCode("123", 1);
         assertTrue(inputted != null);
     }
 
@@ -18,7 +18,7 @@ public class ExpressInCodeTests {
     public void ExpressInCode_tryCatch_Exception() {
         try {
             ExpressInCode expressInCode = new ExpressInCode();
-            String inputted = expressInCode.puttingIntoCode("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG", 20);
+            String inputted = expressInCode.putIntoCode("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG", 20);
         } catch (Exception ex) {
             assertEquals("The shift key must be between 1 and 25", ex.getMessage());
         }
@@ -27,7 +27,7 @@ public class ExpressInCodeTests {
     @Test
     public void ExpressInCode_codesText_String() throws Exception {
         ExpressInCode expressInCode = new ExpressInCode();
-        String inputted = expressInCode.puttingIntoCode("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG", 23);
+        String inputted = expressInCode.putIntoCode("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG", 23);
         assertEquals("QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD", inputted);
     }
 
