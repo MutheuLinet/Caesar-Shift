90d5c3cf704159885abb69429ef9aa9e797b2bfc
MutheuLinet <muneenilinet@gmail.com>
2022-04-24 15:33:46 +0300

    Add test feature to throw exception if the shiftKey is not within the range of 1 to 25

--- src/test/java/shiftTests/ExpressInCodeTests.java
+++ src/test/java/shiftTests/ExpressInCodeTests.java
@@ -1,4 +1,18 @@
 package shiftTests;
 
+import org.junit.jupiter.api.Test;
+import shift.ExpressInCode;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class ExpressInCodeTests {
+    @Test
+    public void ExpressInCode_onlyActualStrings_Exception() throws Exception {
+        ExpressInCode expressInCode = new ExpressInCode();
+        String inputted = expressInCode.puttingIntoCode("123", 20);
+        assertTrue(inputted != null);
+    }
+
+
 }
