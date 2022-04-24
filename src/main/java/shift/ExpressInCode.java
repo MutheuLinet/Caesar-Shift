a8408daa47a95838f8a2f8225dc04f5406535637
MutheuLinet <muneenilinet@gmail.com>
2022-04-24 17:00:11 +0300

    Rename puttingToCode to putToCode

--- src/main/java/shift/ExpressInCode.java
+++ src/main/java/shift/ExpressInCode.java
@@ -3,7 +3,7 @@ package shift;
 public class ExpressInCode {
     private String expressedInCode = "";
 
-    public String puttingIntoCode(String regularText, int shiftKey) throws Exception {
+    public String putIntoCode(String regularText, int shiftKey) throws Exception {
         int l = regularText.length();
 
         if (shiftKey < 1 || shiftKey > 25) throw new Exception("The shift key must be between 1 and 25");
