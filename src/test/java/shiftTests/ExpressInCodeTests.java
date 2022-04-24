4c3df033cc06d5f9dc9bf5ee0bccd5f011db01eb
MutheuLinet <muneenilinet@gmail.com>
2022-04-24 16:00:42 +0300

    Add test to check expected coded text given a certain shift key

--- src/test/java/shiftTests/ExpressInCodeTests.java
+++ src/test/java/shiftTests/ExpressInCodeTests.java
@@ -10,9 +10,25 @@ public class ExpressInCodeTests {
     @Test
     public void ExpressInCode_onlyActualStrings_Exception() throws Exception {
         ExpressInCode expressInCode = new ExpressInCode();
-        String inputted = expressInCode.puttingIntoCode("123", 20);
+        String inputted = expressInCode.puttingIntoCode("123", 1);
         assertTrue(inputted != null);
     }
 
+    @Test
+    public void ExpressInCode_tryCatch_Exception() {
+        try {
+            ExpressInCode expressInCode = new ExpressInCode();
+            String inputted = expressInCode.puttingIntoCode("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG", 20);
+        } catch (Exception ex) {
+            assertEquals("The shift key must be between 1 and 25", ex.getMessage());
+        }
+    }
+
+    @Test
+    public void ExpressInCode_codesText_String() throws Exception {
+        ExpressInCode expressInCode = new ExpressInCode();
+        String inputted = expressInCode.puttingIntoCode("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG", 23);
+        assertEquals("QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD", inputted);
+    }
 
 }
