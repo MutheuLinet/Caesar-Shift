ed21aa6a11d249dc273d6efdad357e62c8fe05c3
MutheuLinet <muneenilinet@gmail.com>
2022-04-24 17:03:59 +0300

    Add feature to throw exception if the shiftKey is not within the range of 1 to 25

--- src/main/java/shift/Decode.java
+++ src/main/java/shift/Decode.java
@@ -1,4 +1,17 @@
 package shift;
 
 public class Decode {
+
+    private String decodedText = "";
+
+    public String undoCode(String codedText, int shiftKey) throws Exception{
+        if (shiftKey < 1 || shiftKey > 25) throw new Exception("The shift key must be between 1 and 25");
+
+        int l = codedText.length();
+
+        for (int i=0; i<l; i++){
+
+        }
+        return decodedText;
+    }
 }
